f5794fbe4746f7c123bc4ad760f03688140c5eec
Karlton Shoemaker <karltonshoemaker@gmail.com>
2021-02-22 20:23:12 -0500

    Experimented with adding ways to make a second pizza. Noticed issue with calculating price with current program where if the topping price is modified on the Sicilian Half Sheet, it will also change the topping price on any other pizzas. Will likely have to make a running tally.

---
+++ PizzaGenerator/Crust.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaGenerator
+{
+    class Crust : IIngredient
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int PricePerTopping { get; set; }
+
+        public Crust()
+        {
+
+        }
+
+        public Crust(string name, int price, int pricePerTopping)
+        {
+            Name = name;
+            Price = price;
+            PricePerTopping = pricePerTopping;
+        }
+
+        public List<Crust> ListOfCrusts(List<Crust> crusts)
+        {
+            crusts.Add(new Crust("American Pan", 10, 2));
+            crusts.Add(new Crust("Detroit Crunch", 10, 2));
+            crusts.Add(new Crust("Thin & Square", 10, 2));
+            crusts.Add(new Crust("Sicilian Half Sheet", 15, 4));
+
+            return crusts;
+        }
+    }
+}
--- PizzaGenerator/Program.cs
+++ PizzaGenerator/Program.cs
@@ -8,18 +8,18 @@ namespace PizzaGenerator
         static void Main(string[] args)
         {
             //This section instantiates necessary instances of all classes and calls the methods that generate the lists of ingredients
-            PizzaStyle pizzaStyle = new PizzaStyle();
+            Crust crust = new Crust();
             Topping topping = new Topping();
-            List<PizzaStyle> pizzaStyles = new List<PizzaStyle>();
+            List<Crust> crusts = new List<Crust>();
             List<Topping> toppings = new List<Topping>();
 
-            pizzaStyle.ListOfPizzaStyles(pizzaStyles);
+            crust.ListOfCrusts(crusts);
             topping.ListOfToppings(toppings);
 
             //This section creates all the randomization of the ingredients
             //The while loop ensures that the same ingredient is not used twice
             Random random = new Random();
-            int randomPizzaStyle = random.Next(pizzaStyles.Count);
+            int randomPizzaStyle = random.Next(crusts.Count);
             int randomTopping = random.Next(toppings.Count);
             int randomTopping2 = random.Next(toppings.Count);
 
@@ -30,9 +30,9 @@ namespace PizzaGenerator
 
             //This section adds all of the randomly generated ingredients into one list that constitutes a pizza
             List<IIngredient> pizza = new List<IIngredient>();
-            pizza.Add(pizzaStyles[randomPizzaStyle]);
+            pizza.Add(crusts[randomPizzaStyle]);
             pizza.Add(toppings[randomTopping]);
-            pizza.Add(toppings[randomTopping2]);
+            pizza.Add(toppings[randomTopping2]); 
 
             //This foreach loop accounts for the fact that the price per topping for the Sicilian Half Sheet size is different from the price per topping for all other sizes supported
             //It works by setting the price for all ingredients to the per topping price of 4, and then it resets the pizza base price to 15
@@ -48,9 +48,38 @@ namespace PizzaGenerator
                 }
             }
 
+            List<IIngredient> order = new List<IIngredient>();
+
+            foreach (var item in pizza)
+            {
+                order.Add(item);
+            }
+
+            List<IIngredient> pizza2 = new List<IIngredient>();
+            pizza2.Add(crusts[3]);
+            pizza2.Add(toppings[5]);
+            pizza2.Add(toppings[6]);
+
+            foreach (var ingredient in pizza2)
+            {
+                if (ingredient.Name == "Sicilian Half Sheet")
+                {
+                    foreach (var toppingOnPizza in pizza2)
+                    {
+                        toppingOnPizza.Price = 4;
+                    }
+                    ingredient.Price = 15;
+                }
+            }
+
+            foreach (var item in pizza2)
+            {
+                order.Add(item);
+            }
+
             //This foreach loop displays which ingredients have been randomly selected and adds the price together
             int totalPrice = 0;
-            foreach (var ingredient in pizza)
+            foreach (var ingredient in order)
             {
                 Console.WriteLine(ingredient.Name);
                 //Console.WriteLine(ingredient.Price);
